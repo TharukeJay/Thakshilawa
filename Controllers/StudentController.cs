4dbf3d9a0eb9c19bf7aa913d1b9b761416131a47
Tharuke Jayawardhane <tharuerox@hotmail.com>
2021-01-09 20:14:35 +0530

    create student controller

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Thakshilawa.ViewModels;
+using XYZLaundry.Data;
+using XYZLaundry.Models;
+
+namespace Thakshilawa.Controllers
+{
+    public class StudentController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public StudentController(UserManager<ApplicationUser> userManager, ApplicationDbContext context)
+        {
+            _userManager = userManager;
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var students = await _context.Student
+                .Select(s => new studentViewModel
+                {
+                    StudentName = s.StudentName,
+                    NICNo = s.NICNo,
+                    DateofBirth = s.DateofBirth,
+                    School = s.School,
+                    Grade = s.Grade,
+                    ContactNumber = s.ContactNumber
+                }).ToListAsync();
+
+            return View(students);
+        }
+    }
+}
---
+++ ViewModels/studentViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Thakshilawa.ViewModels
+{
+    public class studentViewModel
+    {
+        [Display(Name = "Student Name")]
+        public string StudentName { get; set; }
+
+        [Display(Name = "NIC No")]
+        public string NICNo { get; set; }
+
+        [Display(Name = "Date of Birth")]
+        public DateTime DateofBirth { get; set; }
+
+
+        [Display(Name = "School")]
+        public string School { get; set; }
+
+
+        [Display(Name = "Grade")]
+        public string Grade { get; set; }
+
+        [Display(Name = "Contact Number")]
+        public int ContactNumber { get; set; }
+    }
+}
