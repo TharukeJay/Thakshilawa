3c7b5a3faf21dc0411d334988b338e5aad368e13
Tharuke Jayawardhane <tharuerox@hotmail.com>
2021-01-10 02:02:05 +0530

    cafeteria controller completed

---
+++ ViewModels/cafeteriaViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Thakshilawa.ViewModels
+{
+    public class cafeteriaViewModel
+    {
+        public int Id { get; set; }
+        public int EmployeeID { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public Decimal Income { get; set; }
+
+        public Decimal Expenditure { get; set; }
+    }
+}
