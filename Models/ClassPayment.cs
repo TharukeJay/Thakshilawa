970cbf007918edb6381d1f198e13e8c96054e9bf
Tharuke Jayawardhane <tharuerox@hotmail.com>
2021-01-09 16:13:17 +0530

    Adding ClassPayment model

---
+++ Models/ClassPayment.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using XYZLaundry.Models;
+
+namespace Thakshilawa.Models
+{
+    public class ClassPayment : ModelBase
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        public int ClassID { get; set; }
+    }
+}
