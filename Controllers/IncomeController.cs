448888a393200eea134fa21e58b11ead1253eaa5
Nisal <nisal.uduwelaarachchi@cinglevue.com>
2021-01-10 10:46:26 +0530

    Income related changes

--- Controllers/IncomeController.cs
+++ Controllers/IncomeController.cs
@@ -1,13 +1,24 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using XYZLaundry.Data;
+using XYZLaundry.Models;
 
 namespace Thakshilawa.Controllers
 {
     public class IncomeController : Controller
     {
+        private readonly XYZLaundry.Data.ApplicationDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public IncomeController(UserManager<ApplicationUser> userManager, ApplicationDbContext context)
+        {
+            _userManager = userManager;
+            _context = context;
+        }
         public IActionResult Index()
         {
             return View();
